3fec1bb021a7166ee6259a3d1fd32d21188dba34
Rhonstin <rhonstin@gmail.com>
2018-12-24 00:47:10 +0300

    Update RPGInventory.py

--- RPGInventory/RPGInventory.py
+++ RPGInventory/RPGInventory.py
@@ -1,4 +1,5 @@
 inv = {"gold coin" : 42, "rope" : 1, "torch" : 6, "dagger": 1, "arrow" : 12}
+dragonLoot = ["gold coin", "dagger", "gold coin", "gold coin", "ruby"]
 def displayInventory(inv):
     print("Inventory:")
     items = 0
@@ -6,4 +7,9 @@ def displayInventory(inv):
         print(str(v) + " " + k)
         items += v
     print("Tota; numver of items: " + str(items))
+def addToInventroty(inv, addedItems):
+    for item in dragonLoot:
+        inv.setdefault(item,0)
+        inv[item] = inv[item]+1
+addToInventroty(inv,dragonLoot)
 displayInventory(inv)
